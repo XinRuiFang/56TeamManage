c4836209daad4cf72787fefa7e8f283079ac661d
XinRuiFang <abcd02200220@163.com>
2014-11-20 19:50:54 +0800

    第七次提交 新增了忘记密码找回功能

--- 56team/56Team_manage/56Team_manage/Controllers/LoginController.cs
+++ 56team/56Team_manage/56Team_manage/Controllers/LoginController.cs
@@ -54,7 +54,132 @@ namespace _56Team_manage.Controllers
                 return View();
             }
         }
-       
-       
+        [HttpGet]
+        public ActionResult PwdFind()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult PwdFind(FormCollection form)
+        {
+            string vcode = form["vcode"];
+            if (Session["vCode"].ToString() == vcode)
+            {
+                string userName = form["userName"];
+                if (userName != null && userName != "")
+                {
+                    if (BLL.GetDataBaseModel.GetModelBySession(userName) != null)
+                    {
+                        Model.C56rms_user user = BLL.GetDataBaseModel.GetModelBySession(userName);
+                        if (user.user_psw_quest != "" && user.user_psw_quest != null)
+                        {
+                            Session["Name"] = userName;
+                            ViewBag.Quest = user.user_psw_quest;
+                            return View("PwdGet");
+                        }
+                        else
+                        {
+                            Response.Write("<script>alert('对不起，您没有设置密码提示问题，请联系管理员！');</script>");
+                            return View();
+                        }
+                    }
+                    else
+                    {
+                        Response.Write("<script>alert('用户不存在！');</script>");
+                        return View();
+                    }
+                    
+                }
+                else
+                {
+                    Response.Write("<script>alert('请输入用户名！');</script>");
+                    return View();
+                }
+                
+            }
+            else
+            {
+                Response.Write("<script>alert('验证码错误！');</script>");
+                return View();
+            }
+        }
+        [HttpGet]
+        public ActionResult PwdGet()
+        {            
+            return View();
+        }
+        [HttpPost]
+        public ActionResult PwdGet(FormCollection form)
+        {
+            string vcode = form["vcode"];
+            if (Session["vCode"].ToString() == vcode)
+            {
+                string answer = form["answer"];
+                if (answer != null && answer != "")
+                {
+                    Model.C56rms_user user = BLL.GetDataBaseModel.GetModelBySession(Session["Name"].ToString());
+                    if (user.user_psw_answ == answer)
+                    {
+                        return View("ModifPwd");
+                    }
+                    else
+                    {
+                        Response.Write("<script>alert('答案错误！');</script>");
+                        return View("PwdFind");
+                    }
+                }
+                else
+                {
+                    Response.Write("<script>alert('未填写答案！');</script>");
+                    return View("PwdFind");
+                }
+                
+            }
+            else
+            {
+                Response.Write("<script>alert('验证码错误！');</script>");
+                return View("PwdFind");
+            }
+        }
+        [HttpGet]
+        public ActionResult ModifPwd()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult ModifPwd(FormCollection form)
+        {
+            string vcode = form["vcode"];
+            if (Session["vCode"].ToString() == vcode)
+            {
+                string newPwd = form["NewPwd"];
+                string newPwdSec = form["NewPwdSec"];
+                if (newPwd == newPwdSec)
+                {
+                    Model.C56rms_user user = BLL.GetDataBaseModel.GetModelBySession(Session["Name"].ToString());
+                    user.password = newPwd;
+                    if (BLL.SetDataBaseModel.SetData(user))
+                    {
+                        Response.Write("<script>alert('修改成功！');</script>");
+                        return View("Index");
+                    }
+                    else
+                    {
+                        Response.Write("<script>alert('修改失败！');</script>");
+                        return View("Index");
+                    }
+                }
+                else
+                {
+                    Response.Write("<script>alert('两次输入的密码不一致！');</script>");
+                    return View();
+                }
+            }
+            else
+            {
+                Response.Write("<script>alert('验证码错误！');</script>");
+                return View();
+            }
+        }
     }
 }
---
+++ 56team/56Team_manage/Backup/56Team_manage/Controllers/LoginController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Model;
+
+namespace _56Team_manage.Controllers
+{
+    public class LoginController : Controller
+    {
+        //
+        // GET: /Login/
+        [HttpGet]
+        public ActionResult Index()
+        {                     
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(FormCollection form)
+        {
+            string userName = form["userName"];
+            string userPassword = form["userPwd"];
+            string vcode = form["vcode"];
+            if (Session["vCode"].ToString () == vcode)
+            {
+                Model.C56rms_user user = new Model.C56rms_user();
+                user.user_name = userName;
+                user.password = userPassword;
+                if (BLL.LoginBLL.GetUserInformtion(user))
+                {
+                    Model.C56rms_user userModel = BLL.GetDataBaseModel.GetModel(user);
+                    Session["id"] = userModel.user_id;
+                    Session["Name"] = userModel.user_name;                                                                                 
+                    
+                    if (userModel.user_major != null)
+                    {
+                        Session["Major"] = userModel.user_major.ToString();
+                    }
+                    else
+                    {
+                        Session["Major"] = "无";
+                    }
+                    if (userModel.user_department != null)
+                    {
+                        Session["Department"] = userModel.user_department.ToString();
+                    }
+                    else
+                    {
+                        Session["Department"] = "无";
+                    }
+                    if (userModel.user_grade != null)
+                    {
+                        Session["Grade"] = userModel.user_grade;
+                    }
+                    else
+                    {
+                        Session["Grade"] = "无";
+                    }
+                    switch (userModel.user_limit)
+                    {
+                        case 0: Session["Limit"] = "一般用户"; return View("~/views/Home/Index.cshtml");
+                        case 1: Session["Limit"] = "组员"; return View("~/views/Home/Index.cshtml");
+                        case 2: Session["Limit"] = "组长"; return View("~/views/Home/Index.cshtml");
+                        case 3: Session["Limit"] = "管理员"; return View("~/views/Home/Index.cshtml");
+                        default: Session["Limit"] = "未知"; return View("~/views/Home/Index.cshtml");
+                    }
+                   
+                }
+                else
+                {
+                    Response.Write("<script>alert('登陆失败！');</script>");
+                    return View();
+                }
+            }
+            else
+            {
+                Response.Write("<script>alert('验证码错误！');</script>");
+                return View();
+            }
+        }
+       
+       
+    }
+}
